8e2b1c093282a7db85b6e8b30e23fada070e31b6
Clint Banzhaf <clint@banzhaf.pro>
2022-08-25 19:02:36 +0200

    OSX Build: Enable all instructions available on IvyBridge (oldest 10.15 CPU)

--- build/make/platforms/osx-all-osx-x64.mk
+++ build/make/platforms/osx-all-osx-x64.mk
@@ -1,3 +1,25 @@
+
+# MIN OSX: 10.15
+# MIN CPU: Ivy Bridge
+#
+# See: https://support.apple.com/en-us/HT210222
+#
+# | Min. Model  | Released   | CPU                | Architecture |                                                                                                               |
+# |-------------|------------|--------------------|--------------|
+# | MacBook     | early 2015 | Core M-5Y51        | Broadwell    |
+# | MacBook Air | mid   2012 | Core i5-3427U      | Ivy Bridge   |
+# | MacBook Pro | mid   2012 | Core i5-3210M      | Ivy Bridge   |
+# | iMac Pro    | late  2017 | Xeon W-2140B       | Skylake      |
+# | iMac        | late  2012 | Core i5-3330S      | Ivy Bridge   |
+# | Mac mini    | late  2012 | Core i5-3210M      | Ivy Bridge   |
+# | Mac Pro     | late  2013 | Xeon E5-1620v2     | Ivy Bridge   |
+# |-------------|------------|--------------------|--------------|
+OSXMINVERSION   = 10.15
+OSXINSTRUCTIONS = \
+	-mmmx -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 \
+	-mpopcnt -mcx16 -msahf -mfxsr -mavx -mxsave -mpclmul \
+	-mfsgsbase -mrdrnd -mf16c -maes    
+
 # Generic
 EXTBIN     = 
 EXTLIB     = .a
@@ -6,10 +28,10 @@ EXTPDB     = .pdb
 OBJDIR     = obj/osx-x64-$(MODE)
 LIBDIR     = lib/osx-x64
 BINDIR     = bin/osx-x64
-DISTDIR    = ../../dist/osx-10.15
+DISTDIR    = ../../dist/osx-$(OSXMINVERSION)
 TARGET     = x86_64-apple-darwin19.6.0
-MINVER     = -mmacosx-version-min=10.15
-CPUFLAGS   = -march=x86-64 -mtune=generic
+MINVER     = -mmacosx-version-min=$(OSXMINVERSION)
+CPUFLAGS   = -march=x86-64 -mtune=generic $(OSXINSTRUCTIONS)
 DEFINES    = 
 INCLUDES   = 
 CXX        = clang++
