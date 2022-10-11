e81f72c2b6e5916dd6964986ff685aca091c0eb1
Clint Banzhaf <clint@banzhaf.pro>
2022-10-11 10:14:51 +0200

    test

--- build/make/CppCore.Test.mk
+++ build/make/CppCore.Test.mk
@@ -135,7 +135,12 @@ ifeq ($(TARGET_OS),android)
 	echo DEVICE
 	$(AVDMANAGER) list device
 
-#	$(AVDMANAGER) create avd --force --name testAVD --abi google_apis/x86 --package 'system-images;android-29;google_apis;x86'
+	$(AVDMANAGER) create avd --force --name testX86 --abi google_apis/x86 --package 'system-images;android-21;google_apis;x86'
+	$(AVDMANAGER) create avd --force --name testX64 --abi google_apis/x86_64 --package 'system-images;android-21;google_apis;x86_64'
+
+	echo AVD
+	$(AVDMANAGER) list avd
+	
 #	start "" $(EMULATOR) -avd testAVD
 #	timeout 30
 	
