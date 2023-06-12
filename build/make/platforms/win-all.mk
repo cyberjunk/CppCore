56007b6a3a565cc75c75e1bbb0c997f26d453b90
Clint Banzhaf <clint@banzhaf.pro>
2023-06-12 22:09:33 +0200

    test

--- build/make/platforms/win-all.mk
+++ build/make/platforms/win-all.mk
@@ -2,27 +2,27 @@
 
 # Delete Files in Folder by Pattern
 define deletefiles
-	del /s /q $(subst /,\,$(1))\$(2) >nul 2>&1 & exit 0
+	-$(shell del /s /q $(subst /,\,$(1))\$(2) >nul 2>&1)
 endef
 
 # Copy Files between Folders by Pattern
 define copyfiles
-	copy /Y $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1 & exit 0
+	-$(shell copy /Y $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1)
 endef
 
 # Copy Files recursively
 define copyfilesrecursive
-	xcopy /Y /E /H $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1 & exit 0
+	-$(shell xcopy /Y /E /H $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1)
 endef
 
 # Recursively remove folder
 define rmdir	
-	if exist $(subst /,\,$(1)) rd /s /q $(subst /,\,$(1)) >nul 2>&1
+	-$(shell if exist $(subst /,\,$(1)) rd /s /q $(subst /,\,$(1)) >nul 2>&1)
 endef
 
 # Create folder and all sub folders
 define mkdir
-	if not exist $(subst /,\,$(1)) mkdir $(subst /,\,$(1)) >nul 2>&1
+	-$(shell if not exist $(subst /,\,$(1)) mkdir $(subst /,\,$(1)) >nul 2>&1)
 endef
 
 # Replace string occurrences in file
@@ -32,27 +32,27 @@ endef
 
 # Create APPX unencrypted
 define makepkg
-	MakeAppx.exe build /o /h SHA256 /f $(subst /,\,$(1)) /op $(subst /,\,$(2)) >nul 2>&1
+	$(shell MakeAppx.exe build /o /h SHA256 /f $(subst /,\,$(1)) /op $(subst /,\,$(2)) >nul 2>&1)
 endef
 
 # Create APPXBUNDLE
 define makebundle
-	MakeAppx.exe bundle /o /d $(subst /,\,$(1)) /p $(subst /,\,$(2)) >nul 2>&1
+	$(shell MakeAppx.exe bundle /o /d $(subst /,\,$(1)) /p $(subst /,\,$(2)) >nul 2>&1)
 endef
 
 # Sign File with pfx without password
 define sign
-	if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) $(subst /,\,$(1)) >nul 2>&1
+	$(shell if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) $(subst /,\,$(1)) >nul 2>&1)
 endef
 
 # Sign File with pfx with password
 define signp
-	if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) /p $(3) $(subst /,\,$(1)) >nul 2>&1
+	$(shell if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) /p $(3) $(subst /,\,$(1)) >nul 2>&1)
 endef
 
 # Create .pri resources
 define makepri
-	MakePri.exe new /v /o /cf $(subst /,\,$(1)) /pr $(subst /,\,$(2)) /of $(subst /,\,$(3)) /IndexName $(4)
+	$(shell MakePri.exe new /v /o /cf $(subst /,\,$(1)) /pr $(subst /,\,$(2)) /of $(subst /,\,$(3)) /IndexName $(4))
 endef
 
 # Create ZIP
