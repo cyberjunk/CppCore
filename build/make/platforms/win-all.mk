96dbb73fbc535a6ad16adf129e956a3c52b6e6c8
Clint Banzhaf <clint@banzhaf.pro>
2023-06-13 18:53:15 +0200

    test

--- build/make/platforms/win-all.mk
+++ build/make/platforms/win-all.mk
@@ -2,62 +2,27 @@
 
 # Delete Files in Folder by Pattern
 define deletefiles
-	-$(shell del /s /q $(subst /,\,$(1))\$(2) >nul 2>&1)
+	powershell "Remove-Item $(1)/$(2) -Force"
 endef
 
 # Copy Files between Folders by Pattern
 define copyfiles
-	$(shell copy /Y $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1)
+	powershell "Copy-Item $(1) -Destination $(2) -Force"
 endef
 
 # Copy Files recursively
 define copyfilesrecursive
-	$(shell xcopy /Y /E /H $(subst /,\,$(1)) $(subst /,\,$(2)) >nul 2>&1)
+	powershell "Copy-Item $(1) -Destination $(2) -Force -Recurse"
 endef
 
 # Recursively remove folder
-define rmdir	
-	$(shell if exist $(subst /,\,$(1)) rd /s /q $(subst /,\,$(1)) >nul 2>&1)
+define rmdir
+	powershell "Remove-Item $(1) -Force -Recurse"
 endef
 
 # Create folder and all sub folders
 define mkdir
-	$(shell if not exist $(subst /,\,$(1)) mkdir $(subst /,\,$(1)) >nul 2>&1)
-endef
-
-# Replace string occurrences in file
-define replace
-	powershell -Command "(gc $(subst /,\,$(1))) -replace '$(subst /,\,$(2))', '$(subst /,\,$(3))' | Out-File -encoding UTF8 $(subst /,\,$(4))"
-endef
-
-# Create APPX unencrypted
-define makepkg
-	$(shell MakeAppx.exe build /o /h SHA256 /f $(subst /,\,$(1)) /op $(subst /,\,$(2)) >nul 2>&1)
-endef
-
-# Create APPXBUNDLE
-define makebundle
-	$(shell MakeAppx.exe bundle /o /d $(subst /,\,$(1)) /p $(subst /,\,$(2)) >nul 2>&1)
-endef
-
-# Sign File with pfx without password
-define sign
-	$(shell if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) $(subst /,\,$(1)) >nul 2>&1)
-endef
-
-# Sign File with pfx with password
-define signp
-	$(shell if exist $(subst /,\,$(1)) SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) /p $(3) $(subst /,\,$(1)) >nul 2>&1)
-endef
-
-# Create .pri resources
-define makepri
-	$(shell MakePri.exe new /v /o /cf $(subst /,\,$(1)) /pr $(subst /,\,$(2)) /of $(subst /,\,$(3)) /IndexName $(4))
-endef
-
-# Create ZIP
-define makezip
-	powershell Compress-Archive -Force $(1) $(2)
+	mkdir $(subst /,\,$(1))
 endef
 
 # Move Folder or File
@@ -68,4 +33,39 @@ endef
 # Sleep n seconds
 define sleep
 	powershell Start-Sleep -Seconds $(1)
+endef
+
+# Replace string occurrences in file
+define replace
+	powershell -Command "(gc $(subst /,\,$(1))) -replace '$(subst /,\,$(2))', '$(subst /,\,$(3))' | Out-File -encoding UTF8 $(subst /,\,$(4))"
+endef
+
+# Create APPX unencrypted
+define makepkg
+	MakeAppx.exe build /o /h SHA256 /f $(subst /,\,$(1)) /op $(subst /,\,$(2))
+endef
+
+# Create APPXBUNDLE
+define makebundle
+	MakeAppx.exe bundle /o /d $(subst /,\,$(1)) /p $(subst /,\,$(2))
+endef
+
+# Sign File with pfx without password
+define sign
+	SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) $(subst /,\,$(1))
+endef
+
+# Sign File with pfx with password
+define signp
+	SignTool.exe sign /a /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /f $(subst /,\,$(2)) /p $(3) $(subst /,\,$(1))
+endef
+
+# Create .pri resources
+define makepri
+	MakePri.exe new /v /o /cf $(subst /,\,$(1)) /pr $(subst /,\,$(2)) /of $(subst /,\,$(3)) /IndexName $(4)
+endef
+
+# Create ZIP
+define makezip
+	powershell Compress-Archive -Force $(1) $(2)
 endef
