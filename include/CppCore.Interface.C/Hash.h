60d98c50013ba1e7db116c29b54a45d55154082e
Clint Banzhaf <clint@banzhaf.pro>
2023-07-22 19:03:32 +0200

    wip

--- include/CppCore.Interface.C/Hash.h
+++ include/CppCore.Interface.C/Hash.h
@@ -13,8 +13,8 @@ extern "C" {
    CPPCORE_EXPORT cppcore_md5* cppcore_md5_init();
    CPPCORE_EXPORT void cppcore_md5_destroy(cppcore_md5* md5);
    CPPCORE_EXPORT void cppcore_md5_reset(cppcore_md5* md5);
-
    CPPCORE_EXPORT void cppcore_md5_step(cppcore_md5* md5, void* data, size_t len);
+   CPPCORE_EXPORT void cppcore_md5_finish(cppcore_md5* md5, void* hash, size_t len);
 
 #ifdef __cplusplus
 }
--- src/CppCore.Debug/main.cpp
+++ src/CppCore.Debug/main.cpp
@@ -3,11 +3,15 @@
 
 #include <CppCore.Interface.C/Hash.h>
 
+uint64_t d[2];
+
 int main()
 {
    cppcore_md5* md = cppcore_md5_init();
 
    cppcore_md5_step(md, md, 8);
+   cppcore_md5_finish(md, d, sizeof(d));
+
    cppcore_md5_reset(md);
    cppcore_md5_destroy(md);
 
