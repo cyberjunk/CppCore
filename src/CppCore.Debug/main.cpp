7b1dbdef358da1724d674ff775d153eba870a07f
Clint Banzhaf <clint@banzhaf.pro>
2023-07-28 00:15:35 +0200

    test

--- src/CppCore.Debug/main.cpp
+++ src/CppCore.Debug/main.cpp
@@ -3,12 +3,16 @@
 
 #include <CppCore.Interface.C/Crypto.h>
 
-uint64_t d[2];
+uint64_t d[1024];
 
-uint64_t uint512[8];
+//uint64_t uint512[8];
+
+char s[8192];
 
 int main()
 {
+   d[0] = 151512415;
+   d[1] = 0;
    /*cppcore_md5* md = cppcore_md5_init();
 
    cppcore_md5_step(md, md, 8);
@@ -17,8 +21,10 @@ int main()
    cppcore_md5_reset(md);
    cppcore_md5_destroy(md);*/
 
-   cppcore_prime512_generate(uint512, 100);
+   //cppcore_prime512_generate(uint512, 100);
 
+   intptr_t a = cppcore_basex_encode8192(d, s, 63, 10, "0123456789", 1);
+   cppcore_basex_decode8192(s, d, 10, "0123456789");
    return 0;
 }
 
