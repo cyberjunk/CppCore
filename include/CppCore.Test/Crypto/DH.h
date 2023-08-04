68a1d77e0cc679032003cae094315da2f636def9
Clint Banzhaf <clint@banzhaf.pro>
2023-08-04 15:58:54 +0200

    add another DH test

--- include/CppCore.Test/Crypto/DH.h
+++ include/CppCore.Test/Crypto/DH.h
@@ -11,13 +11,35 @@ namespace CppCore { namespace Test { namespace Crypto
    {
    public:
       template<typename DH>
-      INLINE static bool test()
+      INLINE static bool test1()
       {
          DH alice;
          DH bob;
 
+         // alices generates and shares prime and constant
+         // both generate a private key
+
          alice.reset();
          bob.reset(alice.p, alice.g);
+         bob.genkey(alice.V);
+         alice.genkey(bob.V);
+
+         if (::memcmp(&alice.k, &bob.k, DH::SIZE) != 0)
+            return false;
+
+         return true;
+      }
+      template<typename DH>
+      INLINE static bool test2()
+      {
+         DH alice;
+         DH bob;
+
+         // both already know prime (7) and constant (4)
+         // both use their predefined key (alice=2, bob=6)
+
+         alice.reset(7U, 4U, 2U);
+         bob.reset(7U, 4U, 6U);
          bob.genkey(alice.V);
          alice.genkey(bob.V);
 
@@ -37,11 +59,17 @@ namespace CppCore { namespace Test { namespace VS { namespace Crypto {
    TEST_CLASS(DH)
    {
    public:
-      TEST_METHOD(DH128)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test<CppCore::DH128>()); }
-      TEST_METHOD(DH256)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test<CppCore::DH256>()); }
-      TEST_METHOD(DH512)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test<CppCore::DH512>()); }
-      //TEST_METHOD(DH1024)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test<CppCore::DH1024>()); }
-      //TEST_METHOD(DH2048)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test<CppCore::DH2048>()); }
+      TEST_METHOD(TEST1DH128)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test1<CppCore::DH128>()); }
+      TEST_METHOD(TEST1DH256)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test1<CppCore::DH256>()); }
+      TEST_METHOD(TEST1DH512)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test1<CppCore::DH512>()); }
+      //TEST_METHOD(TEST1DH1024)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test1<CppCore::DH1024>()); }
+      //TEST_METHOD(TEST1DH2048)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test1<CppCore::DH2048>()); }
+      TEST_METHOD(TEST2DH128)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test2<CppCore::DH128>()); }
+      TEST_METHOD(TEST2DH256)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test2<CppCore::DH256>()); }
+      TEST_METHOD(TEST2DH512)   { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test2<CppCore::DH512>()); }
+      //TEST_METHOD(TEST2DH1024)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test2<CppCore::DH1024>()); }
+      //TEST_METHOD(TEST2DH2048)  { Assert::AreEqual(true, CppCore::Test::Crypto::DH::test2<CppCore::DH2048>()); }
+
    };
 }}}}
 #endif
