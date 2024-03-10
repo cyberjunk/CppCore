88a213ff832f8ed5e66a9851b75c2a026616b382
Clint Banzhaf <clint@banzhaf.pro>
2024-03-10 18:36:18 +0100

    update

--- src/CppCore.Interface.Python/clib.py
+++ src/CppCore.Interface.Python/clib.py
@@ -1,5 +1,10 @@
 import ctypes
 import sys
+import os
 
+if   sys.platform == 'win32':  ext = ".dll"
+elif sys.platform == 'linux':  ext = ".so"
+elif sys.platform == "darwin": ext = ".dylib"
+else: raise ImportError("Unsupported Platform")
 
-lib = ctypes.CDLL("./CppCore.Interface.C.dll")
+lib = ctypes.CDLL(os.path.abspath(os.path.join(os.path.dirname(__file__), "CppCore.Interface.C"+ext)))
