1f8953d4ad6e55122cb1ef46deb9f9d69fd74bd1
Clint Banzhaf <clint@banzhaf.pro>
2024-03-16 17:56:49 +0100

    update

--- src/CppCore.Interface.Python/cppcore/aes.py
+++ src/CppCore.Interface.Python/cppcore/aes.py
@@ -89,63 +89,63 @@ lib.cppcore_aes256_decrypt_ctr.argtypes = \
 ################################################################
 
 class AES128ECB:
-    h:ctypes.c_void_p
+    _h:ctypes.c_void_p
     def __init__(self):
-        self.h = lib.cppcore_aes128_init()
+        self._h = lib.cppcore_aes128_init()
     def __del__(self):
-        lib.cppcore_aes128_destroy(self.h)
+        lib.cppcore_aes128_destroy(self._h)
     def reset(self, key:bytes):
         if len(key) != 16: raise ValueError("Key must be 16 bytes")
-        lib.cppcore_aes128_reset(self.h, key)
+        lib.cppcore_aes128_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes128_encrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes128_encrypt_ecb(self._h, data, o, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes128_decrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes128_decrypt_ecb(self._h, data, o, n[0])
         return o
 
 class AES192ECB:
-    h:ctypes.c_void_p
+    _h:ctypes.c_void_p
     def __init__(self):
-        self.h = lib.cppcore_aes192_init()
+        self._h = lib.cppcore_aes192_init()
     def __del__(self):
-        lib.cppcore_aes192_destroy(self.h)
+        lib.cppcore_aes192_destroy(self._h)
     def reset(self, key:bytes):
         if len(key) != 24: raise ValueError("Key must be 24 bytes")
-        lib.cppcore_aes192_reset(self.h, key)
+        lib.cppcore_aes192_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes192_encrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes192_encrypt_ecb(self._h, data, o, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes192_decrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes192_decrypt_ecb(self._h, data, o, n[0])
         return o
 
 class AES256ECB:
-    h:ctypes.c_void_p
+    _h:ctypes.c_void_p
     def __init__(self):
-        self.h = lib.cppcore_aes256_init()
+        self._h = lib.cppcore_aes256_init()
     def __del__(self):
-        lib.cppcore_aes256_destroy(self.h)
+        lib.cppcore_aes256_destroy(self._h)
     def reset(self, key:bytes):
         if len(key) != 32: raise ValueError("Key must be 32 bytes")
-        lib.cppcore_aes256_reset(self.h, key)
+        lib.cppcore_aes256_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes256_encrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes256_encrypt_ecb(self._h, data, o, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("ECB requires multiples of 16")
-        lib.cppcore_aes256_decrypt_ecb(self.h, data, o, n[0])
+        lib.cppcore_aes256_decrypt_ecb(self._h, data, o, n[0])
         return o
 
 ################################################################
@@ -153,78 +153,78 @@ class AES256ECB:
 ################################################################
 
 class AES128CBC:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes128_init()
+        self._h = lib.cppcore_aes128_init()
     def __del__(self):
-        lib.cppcore_aes128_destroy(self.h)
+        lib.cppcore_aes128_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 16: raise ValueError("Key must be 16 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes128_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes128_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes128_encrypt_cbc(self.h, data, o, self.ivenc, n[0])
+        lib.cppcore_aes128_encrypt_cbc(self._h, data, o, self._ivenc, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes128_decrypt_cbc(self.h, data, o, self.ivdec, n[0])
+        lib.cppcore_aes128_decrypt_cbc(self._h, data, o, self._ivdec, n[0])
         return o
     
 class AES192CBC:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes192_init()
+        self._h = lib.cppcore_aes192_init()
     def __del__(self):
-        lib.cppcore_aes192_destroy(self.h)
+        lib.cppcore_aes192_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 24: raise ValueError("Key must be 24 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes192_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes192_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes192_encrypt_cbc(self.h, data, o, self.ivenc, n[0])
+        lib.cppcore_aes192_encrypt_cbc(self._h, data, o, self._ivenc, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes192_decrypt_cbc(self.h, data, o, self.ivdec, n[0])
+        lib.cppcore_aes192_decrypt_cbc(self._h, data, o, self._ivdec, n[0])
         return o
 
 class AES256CBC:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes256_init()
+        self._h = lib.cppcore_aes256_init()
     def __del__(self):
-        lib.cppcore_aes256_destroy(self.h)
+        lib.cppcore_aes256_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 32: raise ValueError("Key must be 32 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes256_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes256_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes256_encrypt_cbc(self.h, data, o, self.ivenc, n[0])
+        lib.cppcore_aes256_encrypt_cbc(self._h, data, o, self._ivenc, n[0])
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l); n = divmod(l, 16)
         if n[1]: raise ValueError("CBC requires multiples of 16")
-        lib.cppcore_aes256_decrypt_cbc(self.h, data, o, self.ivdec, n[0])
+        lib.cppcore_aes256_decrypt_cbc(self._h, data, o, self._ivdec, n[0])
         return o
 
 ################################################################
@@ -232,70 +232,70 @@ class AES256CBC:
 ################################################################
 
 class AES128CTR:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes128_init()
+        self._h = lib.cppcore_aes128_init()
     def __del__(self):
-        lib.cppcore_aes128_destroy(self.h)
+        lib.cppcore_aes128_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 16: raise ValueError("Key must be 16 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes128_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes128_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes128_encrypt_ctr(self.h, data, o, self.ivenc, l)
+        lib.cppcore_aes128_encrypt_ctr(self._h, data, o, self._ivenc, l)
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes128_decrypt_ctr(self.h, data, o, self.ivdec, l)
+        lib.cppcore_aes128_decrypt_ctr(self._h, data, o, self._ivdec, l)
         return o
 
 class AES192CTR:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes192_init()
+        self._h = lib.cppcore_aes192_init()
     def __del__(self):
-        lib.cppcore_aes192_destroy(self.h)
+        lib.cppcore_aes192_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 24: raise ValueError("Key must be 24 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes192_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes192_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes192_encrypt_ctr(self.h, data, o, self.ivenc, l)
+        lib.cppcore_aes192_encrypt_ctr(self._h, data, o, self._ivenc, l)
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes192_decrypt_ctr(self.h, data, o, self.ivdec, l)
+        lib.cppcore_aes192_decrypt_ctr(self._h, data, o, self._ivdec, l)
         return o
 
 class AES256CTR:
-    h:ctypes.c_void_p
-    ivenc:bytes=bytes(16)
-    ivdec:bytes=bytes(16)
+    _h:ctypes.c_void_p
+    _ivenc:bytes=bytes(16)
+    _ivdec:bytes=bytes(16)
     def __init__(self):
-        self.h = lib.cppcore_aes256_init()
+        self._h = lib.cppcore_aes256_init()
     def __del__(self):
-        lib.cppcore_aes256_destroy(self.h)
+        lib.cppcore_aes256_destroy(self._h)
     def reset(self, key:bytes, iv:bytes):
         if len(key) != 32: raise ValueError("Key must be 32 bytes")
         if len(iv)  != 16: raise ValueError("IV must be 16 bytes")
-        self.ivenc = bytes(bytearray(iv))
-        self.ivdec = bytes(bytearray(iv))
-        lib.cppcore_aes256_reset(self.h, key)
+        self._ivenc = bytes(bytearray(iv))
+        self._ivdec = bytes(bytearray(iv))
+        lib.cppcore_aes256_reset(self._h, key)
     def encrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes256_encrypt_ctr(self.h, data, o, self.ivenc, l)
+        lib.cppcore_aes256_encrypt_ctr(self._h, data, o, self._ivenc, l)
         return o
     def decrypt(self, data:bytes) -> bytes:
         l = len(data); o = bytes(l)
-        lib.cppcore_aes256_decrypt_ctr(self.h, data, o, self.ivdec, l)
+        lib.cppcore_aes256_decrypt_ctr(self._h, data, o, self._ivdec, l)
         return o
