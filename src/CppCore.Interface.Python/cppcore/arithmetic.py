705368dc70567d68a7abb67f61c6deaf677964e1
Clint Banzhaf <clint@banzhaf.pro>
2024-03-17 09:43:51 +0100

    update

--- src/CppCore.Interface.Python/cppcore/arithmetic.py
+++ src/CppCore.Interface.Python/cppcore/arithmetic.py
@@ -2,6 +2,22 @@ from cppcore.lib.libcppcore import lib
 import ctypes
 import sys
 
+lib.cppcore_uint32_mulmod.argtypes = \
+lib.cppcore_uint64_mulmod.argtypes = \
+lib.cppcore_uint128_mulmod.argtypes = \
+lib.cppcore_uint256_mulmod.argtypes = \
+lib.cppcore_uint512_mulmod.argtypes = \
+lib.cppcore_uint1024_mulmod.argtypes = \
+lib.cppcore_uint2048_mulmod.argtypes = \
+lib.cppcore_uint4096_mulmod.argtypes = \
+lib.cppcore_uint8192_mulmod.argtypes = \
+[
+    ctypes.c_void_p, # a
+    ctypes.c_void_p, # b
+    ctypes.c_void_p, # m
+    ctypes.c_void_p  # r
+]
+
 lib.cppcore_uint32_powmod.argtypes = \
 lib.cppcore_uint64_powmod.argtypes = \
 lib.cppcore_uint128_powmod.argtypes = \
@@ -18,11 +34,74 @@ lib.cppcore_uint8192_powmod.argtypes = \
     ctypes.c_void_p  # r
 ]
 
+def umulmod(a:int, b:int, m:int) -> int:
+    """Calculates (a*b) % m"""
+    if a <  0: raise ValueError("Negative a")
+    if b <  0: raise ValueError("Negative b")
+    if m <= 0: raise ValueError("Negative or zero m")
+    bitlen:int = max(a.bit_length(), b.bit_length(), m.bit_length())
+    if bitlen <= 32:
+        r = bytes(4)
+        a = a.to_bytes(4, byteorder=sys.byteorder)
+        b = b.to_bytes(4, byteorder=sys.byteorder)
+        m = m.to_bytes(4, byteorder=sys.byteorder)
+        lib.cppcore_uint32_mulmod(a,b,m,r)
+    elif bitlen <= 64:
+        r = bytes(8)
+        a = a.to_bytes(8, byteorder=sys.byteorder)
+        b = b.to_bytes(8, byteorder=sys.byteorder)
+        m = m.to_bytes(8, byteorder=sys.byteorder)
+        lib.cppcore_uint64_mulmod(a,b,m,r)
+    elif bitlen <= 128:
+        r = bytes(16)
+        a = a.to_bytes(16, byteorder=sys.byteorder)
+        b = b.to_bytes(16, byteorder=sys.byteorder)
+        m = m.to_bytes(16, byteorder=sys.byteorder)
+        lib.cppcore_uint128_mulmod(a,b,m,r)
+    elif bitlen <= 256:
+        r = bytes(32)
+        a = a.to_bytes(32, byteorder=sys.byteorder)
+        b = b.to_bytes(32, byteorder=sys.byteorder)
+        m = m.to_bytes(32, byteorder=sys.byteorder)
+        lib.cppcore_uint256_mulmod(a,b,m,r)
+    elif bitlen <= 512:
+        r = bytes(64)
+        a = a.to_bytes(64, byteorder=sys.byteorder)
+        b = b.to_bytes(64, byteorder=sys.byteorder)
+        m = m.to_bytes(64, byteorder=sys.byteorder)
+        lib.cppcore_uint512_mulmod(a,b,m,r)
+    elif bitlen <= 1024:
+        r = bytes(128)
+        a = a.to_bytes(128, byteorder=sys.byteorder)
+        b = b.to_bytes(128, byteorder=sys.byteorder)
+        m = m.to_bytes(128, byteorder=sys.byteorder)
+        lib.cppcore_uint1024_mulmod(a,b,m,r)
+    elif bitlen <= 2048:
+        r = bytes(256)
+        a = a.to_bytes(256, byteorder=sys.byteorder)
+        b = b.to_bytes(256, byteorder=sys.byteorder)
+        m = m.to_bytes(256, byteorder=sys.byteorder)
+        lib.cppcore_uint2048_mulmod(a,b,m,r)
+    elif bitlen <= 4096:
+        r = bytes(512)
+        a = a.to_bytes(512, byteorder=sys.byteorder)
+        b = b.to_bytes(512, byteorder=sys.byteorder)
+        m = m.to_bytes(512, byteorder=sys.byteorder)
+        lib.cppcore_uint4096_mulmod(a,b,m,r)
+    elif bitlen <= 8192:
+        r = bytes(1024)
+        a = a.to_bytes(1024, byteorder=sys.byteorder)
+        b = b.to_bytes(1024, byteorder=sys.byteorder)
+        m = m.to_bytes(1024, byteorder=sys.byteorder)
+        lib.cppcore_uint8192_mulmod(a,b,m,r)
+    else: raise ValueError("Too big. Maximum is 8192 bits")
+    return int.from_bytes(r, byteorder=sys.byteorder)
+
 def upowmod(a:int, b:int, m:int) -> int:
     """Calculates (a**b) % m"""
-    if a < 0: raise ValueError("Negative a")
-    if b < 0: raise ValueError("Negative b")
-    if m < 0: raise ValueError("Negative m")
+    if a <  0: raise ValueError("Negative a")
+    if b <  0: raise ValueError("Negative b")
+    if m <= 0: raise ValueError("Negative or zero m")
     bitlen:int = max(a.bit_length(), b.bit_length(), m.bit_length())
     if bitlen <= 32:
         r = bytes(4)
--- src/CppCore.Interface.Python/tests/test_arithmetic.py
+++ src/CppCore.Interface.Python/tests/test_arithmetic.py
@@ -2,12 +2,29 @@ from cppcore.util.maxint import *
 import cppcore.arithmetic
 import random
 
+def helper_umulmod(min:int, max:int, n:int):
+    for i in range(n):
+        a = random.randint(min, max)
+        b = random.randint(min, max)
+        m = random.randint(min, max)
+        assert(cppcore.arithmetic.umulmod(a,b,m) == (a*b)%m)
+
 def helper_upowmod(min:int, max:int, n:int):
     for i in range(n):
         a = random.randint(min, max)
         b = random.randint(min, max)
         m = random.randint(min, max)
         assert(cppcore.arithmetic.upowmod(a,b,m) == pow(a,b,m))
+
+def test_umulmod32():   helper_umulmod(1, MAX32I,   100)
+def test_umulmod64():   helper_umulmod(1, MAX64I,   100)
+def test_umulmod128():  helper_umulmod(1, MAX128I,  100)
+def test_umulmod256():  helper_umulmod(1, MAX256I,  100)
+def test_umulmod512():  helper_umulmod(1, MAX512I,  100)
+def test_umulmod1024(): helper_umulmod(1, MAX1024I, 100)
+def test_umulmod2048(): helper_umulmod(1, MAX2048I, 10)
+def test_umulmod4096(): helper_umulmod(1, MAX4096I, 10)
+def test_umulmod8192(): helper_umulmod(1, MAX8192I, 10)
 
 def test_upowmod32():   helper_upowmod(1, MAX32I,   100)
 def test_upowmod64():   helper_upowmod(1, MAX64I,   100)
