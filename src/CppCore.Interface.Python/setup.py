11a4062b4f445e354647770ed4c9a24915e41298
Clint Banzhaf <clint@banzhaf.pro>
2024-03-23 22:24:34 +0100

    wip

--- src/CppCore.Interface.Python/setup.py
+++ src/CppCore.Interface.Python/setup.py
@@ -1,16 +1,23 @@
-from setuptools import setup
-import sysconfig
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+#import sysconfig
 
-def get_platname():
-    # TODO: Cross-Compile...
-    return sysconfig.get_platform()
+class my_build_ext(build_ext):
+    def build_extensions(self):
+        pass
+
+#def get_platname():
+#    # TODO: Cross-Compile...
+#    return sysconfig.get_platform()
 
 setup(
     include_package_data=True,
     package_data={'': ['libcppcore.dll', 'libcppcore.dylib', 'libcppcore.so']},
-    options={
-        "bdist_wheel": {
-            "plat_name": get_platname(),
-        },
-    },
+    #options={
+    #    "bdist_wheel": {
+    #        "plat_name": get_platname(),
+    #    },
+    #},
+    ext_modules = [Extension("", [])],
+    cmdclass = {'build_ext': my_build_ext},
 )
