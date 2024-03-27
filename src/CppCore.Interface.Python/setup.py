5cbb96d12e33e5d965c23924ffa730a35c273128
Clint Banzhaf <clint@banzhaf.pro>
2024-03-27 22:00:39 +0100

    update

--- src/CppCore.Interface.Python/setup.py
+++ src/CppCore.Interface.Python/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 #import sysconfig
+from packaging.version import Version
+import os
 
 class my_build_ext(build_ext):
     def build_extensions(self):
@@ -10,7 +12,11 @@ class my_build_ext(build_ext):
 #    # TODO: Cross-Compile...
 #    return sysconfig.get_platform()
 
+version_string = os.environ.get("RELEASE_VERSION", "0.0.0.dev0")
+version = Version(version_string)
+
 setup(
+    version=str(version),
     include_package_data=True,
     package_data={'': ['libcppcore.dll', 'libcppcore.dylib', 'libcppcore.so']},
     #options={
