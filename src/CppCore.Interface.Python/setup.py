775fb7c851b5a555dee27c7aa267f9381c49777e
Clint Banzhaf <clint@banzhaf.pro>
2024-03-28 21:58:27 +0100

    update

--- src/CppCore.Interface.Python/setup.py
+++ src/CppCore.Interface.Python/setup.py
@@ -1,27 +1,32 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-#import sysconfig
-#from packaging.version import Version
-
 import os
 
 class my_build_ext(build_ext):
     def build_extensions(self):
         pass
 
+#WIN
+#set VSCMD_ARG_TGT_ARCH=x64
+#set VSCMD_ARG_TGT_ARCH=x86
+#set VSCMD_ARG_TGT_ARCH=arm64
 
+#MAC    
 #export _PYTHON_HOST_PLATFORM="macosx-10.15-universal2"
+
+#LINUX
+#export _PYTHON_HOST_PLATFORM=linux_x86_64
+#export _PYTHON_HOST_PLATFORM=linux_i686
+#export _PYTHON_HOST_PLATFORM=linux_aarch64
+#export _PYTHON_HOST_PLATFORM=linux_armv7hl
 
 #def get_platname():
 #    # TODO: Cross-Compile...
 #    return sysconfig.get_platform()
 
-#version_string = os.environ.get("CPPCORE_VERSION3", "0.0.0")
-#version = Version(version_string)
-
 setup(
     name="CppCore",
-    version=os.environ.get("CPPCORE_VERSION3", "0.0.0"),
+    version=os.environ.get("CPPCORE_VERSION", "0.0.0"),
     description="CppCore for Python",
     author="Clint Banzhaf",
     url="https://github.com/cyberjunk/CppCore",
