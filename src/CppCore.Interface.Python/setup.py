5b939232303b5835752a7716a4e1a860cda6979f
Clint Banzhaf <clint.banzhaf@zds.de>
2024-03-28 20:35:44 +0100

    update

--- src/CppCore.Interface.Python/setup.py
+++ src/CppCore.Interface.Python/setup.py
@@ -1,24 +1,36 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 #import sysconfig
-from packaging.version import Version
+#from packaging.version import Version
+
 import os
 
 class my_build_ext(build_ext):
     def build_extensions(self):
         pass
 
+
+#export _PYTHON_HOST_PLATFORM="macosx-10.15-universal2"
+
 #def get_platname():
 #    # TODO: Cross-Compile...
 #    return sysconfig.get_platform()
 
-version_string = os.environ.get("RELEASE_VERSION", "0.0.0.dev0")
-version = Version(version_string)
+#version_string = os.environ.get("CPPCORE_VERSION3", "0.0.0")
+#version = Version(version_string)
 
 setup(
-    version=str(version),
+    name="CppCore",
+    version=os.environ.get("CPPCORE_VERSION3", "0.0.0"),
+    description="CppCore for Python",
+    author="Clint Banzhaf",
+    url="https://github.com/cyberjunk/CppCore",
+    keywords=[
+        "primes", "aes", "crypto", "encoding", 
+        "hash", "hmac", "pbkdf2", "dh"
+    ],
     include_package_data=True,
-    package_data={'': ['libcppcore.dll', 'libcppcore.dylib', 'libcppcore.so']},
+    package_data={'': ['libcppcore.*']},
     #options={
     #    "bdist_wheel": {
     #        "plat_name": get_platname(),
@@ -26,4 +38,9 @@ setup(
     #},
     ext_modules = [Extension("", [])],
     cmdclass = {'build_ext': my_build_ext},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
