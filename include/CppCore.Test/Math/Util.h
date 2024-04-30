df6e28875bd1d1e85866658f948427d7a8c06229
Clint Banzhaf <clint@banzhaf.pro>
2024-04-30 19:16:19 +0200

    test

--- include/CppCore.Test/Math/Util.h
+++ include/CppCore.Test/Math/Util.h
@@ -1295,17 +1295,19 @@ namespace CppCore { namespace Test { namespace Math
 
       INLINE static bool clmul64to128()
       {
-         uint64_t r[2];
+         struct t { uint64_t l; uint64_t h; };
+         //uint64_t r[2];
+         t r;
          size_t s = sizeof(r);
-         CppCore::clmul((uint64_t)0x0000000000000000ULL, (uint64_t)0x0000000000000000ULL, r); if (r[0] != (uint64_t)0x0000000000000000ULL || r[1] != (uint64_t)0x0000000000000000ULL) return false;
-         CppCore::clmul((uint64_t)0x0000000000000001ULL, (uint64_t)0x0000000000000000ULL, r); if (r[0] != (uint64_t)0x0000000000000000ULL || r[1] != (uint64_t)0x0000000000000000ULL) return false;
-         CppCore::clmul((uint64_t)0x0000000000000000ULL, (uint64_t)0x0000000000000001ULL, r); if (r[0] != (uint64_t)0x0000000000000000ULL || r[1] != (uint64_t)0x0000000000000000ULL) return false;
-         CppCore::clmul((uint64_t)0x0000000000000001ULL, (uint64_t)0x0000000000000001ULL, r); if (r[0] != (uint64_t)0x0000000000000001ULL || r[1] != (uint64_t)0x0000000000000000ULL) return false;
-         CppCore::clmul((uint64_t)0x0000000000000002ULL, (uint64_t)0x0000000000000003ULL, r); if (r[0] != (uint64_t)0x0000000000000006ULL || r[1] != (uint64_t)0x0000000000000000ULL) return false;
-         CppCore::clmul((uint64_t)0x5b477565726f6e5dULL, (uint64_t)0x63746f725d53475dULL, r); if (r[0] != (uint64_t)0x929633d5d36f0451ULL || r[1] != (uint64_t)0x1d4d84c85c3440c0ULL) return false;
-         CppCore::clmul((uint64_t)0x4869285368617929ULL, (uint64_t)0x63746f725d53475dULL, r); if (r[0] != (uint64_t)0x7fa540ac2a281315ULL || r[1] != (uint64_t)0x1bd17c8d556ab5a1ULL) return false;
-         CppCore::clmul((uint64_t)0x5b477565726f6e5dULL, (uint64_t)0x7b5b546573745665ULL, r); if (r[0] != (uint64_t)0xbabf262df4b7d5c9ULL || r[1] != (uint64_t)0x1a2bf6db3a30862fULL) return false;
-         CppCore::clmul((uint64_t)0x4869285368617929ULL, (uint64_t)0x7b5b546573745665ULL, r); if (r[0] != (uint64_t)0xd66ee03e410fd4edULL || r[1] != (uint64_t)0x1d1e1f2c592e7c45ULL) return false;
+         CppCore::clmul((uint64_t)0x0000000000000000ULL, (uint64_t)0x0000000000000000ULL, r); if (r.l != (uint64_t)0x0000000000000000ULL || r.h != (uint64_t)0x0000000000000000ULL) return false;
+         CppCore::clmul((uint64_t)0x0000000000000001ULL, (uint64_t)0x0000000000000000ULL, r); if (r.l != (uint64_t)0x0000000000000000ULL || r.h != (uint64_t)0x0000000000000000ULL) return false;
+         CppCore::clmul((uint64_t)0x0000000000000000ULL, (uint64_t)0x0000000000000001ULL, r); if (r.l != (uint64_t)0x0000000000000000ULL || r.h != (uint64_t)0x0000000000000000ULL) return false;
+         CppCore::clmul((uint64_t)0x0000000000000001ULL, (uint64_t)0x0000000000000001ULL, r); if (r.l != (uint64_t)0x0000000000000001ULL || r.h != (uint64_t)0x0000000000000000ULL) return false;
+         CppCore::clmul((uint64_t)0x0000000000000002ULL, (uint64_t)0x0000000000000003ULL, r); if (r.l != (uint64_t)0x0000000000000006ULL || r.h != (uint64_t)0x0000000000000000ULL) return false;
+         CppCore::clmul((uint64_t)0x5b477565726f6e5dULL, (uint64_t)0x63746f725d53475dULL, r); if (r.l != (uint64_t)0x929633d5d36f0451ULL || r.h != (uint64_t)0x1d4d84c85c3440c0ULL) return false;
+         CppCore::clmul((uint64_t)0x4869285368617929ULL, (uint64_t)0x63746f725d53475dULL, r); if (r.l != (uint64_t)0x7fa540ac2a281315ULL || r.h != (uint64_t)0x1bd17c8d556ab5a1ULL) return false;
+         CppCore::clmul((uint64_t)0x5b477565726f6e5dULL, (uint64_t)0x7b5b546573745665ULL, r); if (r.l != (uint64_t)0xbabf262df4b7d5c9ULL || r.h != (uint64_t)0x1a2bf6db3a30862fULL) return false;
+         CppCore::clmul((uint64_t)0x4869285368617929ULL, (uint64_t)0x7b5b546573745665ULL, r); if (r.l != (uint64_t)0xd66ee03e410fd4edULL || r.h != (uint64_t)0x1d1e1f2c592e7c45ULL) return false;
       #if defined(CPPCORE_CPUFEAT_PCLMUL)
          CppCore::Random::Default64 rnd;
          for (uint32_t i = 0; i < 100; i++)
