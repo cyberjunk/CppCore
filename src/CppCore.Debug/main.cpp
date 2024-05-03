1830d87d82ecc87072aba54a942b0eacd65d1236
Clint Banzhaf <clint@banzhaf.pro>
2024-05-03 14:41:26 +0200

    test

--- src/CppCore.Debug/main.cpp
+++ src/CppCore.Debug/main.cpp
@@ -1,25 +1,42 @@
 #include <CppCore/Root.h>
+#include <CppCore/Math/Util.h>
 
-int main()
+NOINLINE static int clmul64to128()
 {
-   //uint64_t r[2];
    struct t { uint64_t l; uint64_t h; };
    t r;
 
    uint32_t* pr32 = (uint32_t*)&r;
+
    for (size_t i = 0; i < 4; i++)
    {
       *pr32++ = 0U;
    }
 
-   std::cout << r.l << std::endl;
-   std::cout << r.h << std::endl;
+   std::cout << std::hex << "-----------" << std::endl;
 
-   if (r.l != 0 || r.h != 0)
-   {
-      std::cout << "ERROR" << std::endl;
+   size_t s = sizeof(r);
+   std::cout << std::hex << r.l << std::endl;
+   std::cout << std::hex << r.h << std::endl;
+
+   uint64_t a = (uint64_t)0x0000000000000000ULL;
+   uint64_t b = (uint64_t)0x0000000000000000ULL;
+   CppCore::clmul(a, b, r);
+   std::cout << std::hex << r.l << std::endl;
+   std::cout << std::hex << r.h << std::endl;
+
+   if (r.l != (uint64_t)0x0000000000000000ULL || r.h != (uint64_t)0x0000000000000000ULL) {
+      std::cout << std::hex << r.l << std::endl;
+      std::cout << std::hex << r.h << std::endl;
+
       return 1;
    }
 
    return 0;
+}
+
+
+int main()
+{
+   return clmul64to128();
 }
