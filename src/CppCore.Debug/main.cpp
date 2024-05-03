3850c3b4232c2db3ee2cfeadb82086403ba019a5
Clint Banzhaf <clint@banzhaf.pro>
2024-05-03 14:20:40 +0200

    test

--- src/CppCore.Debug/main.cpp
+++ src/CppCore.Debug/main.cpp
@@ -1,8 +1,23 @@
 #include <CppCore/Root.h>
 
-using namespace CppCore;
-
 int main()
 {
+   uint64_t r[2];
+
+   uint32_t* pr32 = (uint32_t*)&r;
+   for (size_t i = 0; i < 4; i++)
+   {
+      *pr32++ = 0U;
+   }
+
+   std::cout << r[0] << std::endl;
+   std::cout << r[1] << std::endl;
+
+   if (r[0] != 0 || r[1] != 0)
+   {
+      std::cout << "ERROR" << std::endl;
+      return 1;
+   }
+
    return 0;
 }
