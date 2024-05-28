0ba63405f471e6496d972c95344129f194ec2089
Clint Banzhaf <clint@banzhaf.pro>
2024-05-28 19:17:02 +0200

    update

--- include/CppCore/Math/BigInt.h
+++ include/CppCore/Math/BigInt.h
@@ -2851,6 +2851,7 @@ namespace CppCore
    {
    public:
       using uint128_bg::uint128_bg;
+      INLINE uint128_tg() { }
       INLINE uint128_tg(const uint64_t l, const uint64_t h = 0ULL) : uint128_bg(uint128_tgd(l, h)) { }
    };
 
@@ -2903,6 +2904,7 @@ namespace CppCore
    {
    public:
       using uint128_bs::uint128_bs;
+      INLINE uint128_ts() { }
       INLINE uint128_ts(const uint64_t l, const uint64_t h = 0ULL) : uint128_bs(uint128_tsd(l, h)) { }
    #if defined(CPPCORE_CPUFEAT_SSE2)
       INLINE uint128_ts(const __m128i& other) : uint128_bs(uint128_tsd(other)) { }
@@ -2963,6 +2965,7 @@ namespace CppCore
    {
    public:
       using uint256_bg::uint256_bg;
+      INLINE uint256_tg() { }
       INLINE uint256_tg(const uint128_tg& l, const uint128_tg& h = 0ULL) : uint256_bg(uint256_tgd(l, h)) { }
       INLINE uint256_tg(const uint64_t v1, const uint64_t v2 = 0, const uint64_t v3 = 0, const uint64_t v4 = 0) : uint256_bg(uint256_tgd(v1, v2, v3, v4)) { }
       INLINE explicit operator const uint128_tg& () const { return d.i128[0]; }
@@ -2992,6 +2995,7 @@ namespace CppCore
    {
    public:
       using uint256_bs::uint256_bs;
+      INLINE uint256_ts() { }
       INLINE uint256_ts(const uint128_ts& l, const uint128_ts& h = 0ULL) : uint256_bs(uint256_tsd(l, h)) { }
       INLINE uint256_ts(const uint64_t v1, const uint64_t v2 = 0, const uint64_t v3 = 0, const uint64_t v4 = 0) :
          uint256_bs(uint256_tsd(v1, v2, v3, v4)) { }
@@ -3030,6 +3034,7 @@ namespace CppCore
    {
    public:
       using uint512_bg::uint512_bg;
+      INLINE uint512_tg() { }
       INLINE uint512_tg(const uint256_tg& l, const uint256_tg& h = 0ULL) : uint512_bg(uint512_tgd(l, h)) { }
       INLINE uint512_tg(
          const uint64_t v1,     const uint64_t v2 = 0, const uint64_t v3 = 0, const uint64_t v4 = 0, 
@@ -3063,6 +3068,7 @@ namespace CppCore
    {
    public:
       using uint512_bs::uint512_bs;
+      INLINE uint512_ts() { }
       INLINE uint512_ts(const uint256_ts& l, const uint256_ts& h = 0ULL) : uint512_bs(uint512_tsd(l, h)) { }
       INLINE uint512_ts(
          const uint64_t v1,     const uint64_t v2 = 0, const uint64_t v3 = 0, const uint64_t v4 = 0, 
@@ -3104,6 +3110,7 @@ namespace CppCore
    {
    public:
       using uint1024_bg::uint1024_bg;
+      INLINE uint1024_tg() { }
       INLINE uint1024_tg(const uint512_tg& l, const uint512_tg& h = 0ULL) : uint1024_bg(uint1024_tgd(l, h)) { }
       INLINE uint1024_tg(
          const uint64_t v1,      const uint64_t v2  = 0, const uint64_t v3  = 0, const uint64_t v4  = 0, 
@@ -3140,6 +3147,7 @@ namespace CppCore
    {
    public:
       using uint1024_bs::uint1024_bs;
+      INLINE uint1024_ts() { }
       INLINE uint1024_ts(const uint512_ts& l, const uint512_ts& h = 0ULL) : uint1024_bs(uint1024_tsd(l, h)) { }
       INLINE uint1024_ts(
          const uint64_t v1,      const uint64_t v2  = 0, const uint64_t v3  = 0, const uint64_t v4  = 0, 
@@ -3180,6 +3188,7 @@ namespace CppCore
    {
    public:
       using uint2048_bg::uint2048_bg;
+      INLINE uint2048_tg() { }
       INLINE uint2048_tg(const uint1024_tg& l, const uint1024_tg& h = 0ULL) : uint2048_bg(uint2048_tgd(l, h)) { }
       INLINE uint2048_tg(
          const uint64_t v1,      const uint64_t v2  = 0, const uint64_t v3  = 0, const uint64_t v4  = 0, 
@@ -3223,6 +3232,7 @@ namespace CppCore
    {
    public:
       using uint2048_bs::uint2048_bs;
+      INLINE uint2048_ts() { }
       INLINE uint2048_ts(const uint1024_ts& l, const uint1024_ts& h = 0ULL) : uint2048_bs(uint2048_tsd(l, h)) { }
       INLINE uint2048_ts(
          const uint64_t v1,      const uint64_t v2  = 0, const uint64_t v3  = 0, const uint64_t v4  = 0, 
