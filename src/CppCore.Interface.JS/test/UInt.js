14f5905cd5313bada3ef775c65a0228e764215dd
Clint Banzhaf <clint@banzhaf.pro>
2024-10-12 22:00:19 +0200

    wip

--- src/CppCore.Interface.JS/test/UInt.js
+++ src/CppCore.Interface.JS/test/UInt.js
@@ -2,6 +2,35 @@ import {
     UInt128 
 } from '../libcppcore.js';
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function getRandomHexStr(max) {
+    const symbols = "0123456789ABCDEF";
+    const len = getRandomInt(max-1) + 1;
+    var str = "0x";
+    for (var i=0; i < len; i++) {
+        str += symbols[getRandomInt(16)];
+    }
+    return str;
+}
+function testAdd(type, bits, maxhexsymbols, iterations) {
+    var a2 = new type();        
+    var b2 = new type();
+    for(var n=0;n<iterations;n++) {
+      var stra = getRandomHexStr(maxhexsymbols);
+      var strb = getRandomHexStr(maxhexsymbols);
+      var a1 = BigInt(stra);
+      var b1 = BigInt(strb);
+      a2.set(stra);
+      b2.set(strb);
+      a1 += b1;
+      a1 = BigInt.asUintN(bits, a1);
+      a2.add(b2);
+      chai.expect(a1.toString()).to.equal(a2.toString());
+    }
+}
+
 describe('UInt', function () {
   describe('UInt128', function () {
     it('add()', function () {
@@ -17,6 +46,9 @@ describe('UInt', function () {
         const c = new UInt128("0")
         a.sub(b);
         chai.expect(indexedDB.cmp(a._buffer, c._buffer)).to.equal(0);
+    });
+    it('test()', function () {
+        testAdd(UInt128, 128, 32, 100);
     });
   });
 });
