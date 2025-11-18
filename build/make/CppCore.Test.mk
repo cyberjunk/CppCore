11b9086c9b4102839c5b05b752133e501679efa4
Clint Banzhaf <clint@banzhaf.pro>
2025-11-18 01:46:55 +0100

    android gpu swiftshader_indirect

--- build/make/CppCore.Test.mk
+++ build/make/CppCore.Test.mk
@@ -159,9 +159,9 @@ ifeq ($(TARGET_OS),android)
 	$(AVDMANAGER) list avd
 	$(ADB) start-server
 ifeq ($(DETECTED_OS),win)
-	start "" $(EMULATOR) -no-window -no-audio -no-snapshot -gpu guest -avd $(NAME)_AVD
+	start "" $(EMULATOR) -no-window -no-audio -no-snapshot -gpu swiftshader_indirect -avd $(NAME)_AVD
 else
-	$(EMULATOR) -no-window -no-audio -no-snapshot -gpu guest -avd $(NAME)_AVD &
+	$(EMULATOR) -no-window -no-audio -no-snapshot -gpu swiftshader_indirect -avd $(NAME)_AVD &
 endif
 	$(ADB) wait-for-any-device
 	$(ADB) devices
