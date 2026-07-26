c83aab24868937b96aa9a33e60de99bd6b224fe5
Clint Banzhaf <clint@banzhaf.pro>
2026-07-26 17:43:08 +0200

    update

--- include/CppCore/Math/Primes.h
+++ include/CppCore/Math/Primes.h
@@ -313,7 +313,7 @@ namespace CppCore
          UINTX2 mem[2];
          for (uint32_t i = 2U; i < ith; i++)
          {
-            CppCore::umul(s, s, r);
+            CppCore::usquare(s, r);
             CppCore::usub(r, (size_t)2U, r);
             CppCore::umod(s, r, n, mem);
          }
@@ -417,7 +417,7 @@ namespace CppCore
 
          // perfect square root test
          CppCore::isqrt(n, t);
-         CppCore::umul(t, t, d);
+         CppCore::usquare(t, d);
          if (CppCore::equal(n, d))
             return Primes::NotPrime;
 
